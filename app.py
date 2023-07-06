a5cf5ad284541148c4aa99604d87d84ddf81e45f
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-06 18:12:38 -0400

    update

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ import sqlalchemy
 import hashlib
 import secrets
 
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
-
 
 def scramble(password: str):
     """Hash and salt the given password"""
@@ -22,30 +19,27 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, username: str, password: str):
-            self.username = username
-            self.password = password
+    def __init__(self, username: str, email: str, password: str):
+        self.username = username
+        self.email = email
+        self.password = password
+
     def serialize(self):
         return {
             'id': self.id,
             'username': self.username,
-    }
+            'email': self.email
+        }
 
 db.create_all()
 
-
-
-
-
-#create a test route
+# create a test route
 @app.route('/test', methods=['GET'])
 def test():
-  return make_response(jsonify({'message': 'test route moved models again enough'}), 200)
-
-
-
+    return make_response(jsonify({'message': 'test route moved models again enough'}), 200)
 
 # create a user
 @app.route('/users', methods=['POST'])
@@ -56,52 +50,26 @@ def create():
         return abort(400)
     u = User(
         username=request.json['username'],
+        email=request.json['email'],
         password=scramble(request.json['password'])
-
     )
     db.session.add(u)
     db.session.commit()
-    return jsonify(u.json())
-
+    return jsonify(u.serialize())
 
 # get all users
 @app.route('/users', methods=['GET'])
 def index():
     users = User.query.all()
-    result = []
-    for u in users:
-        result.append(u.serialize())
-    return jsonify(result)
-
+    return jsonify([user.serialize() for user in users])
 
 # get a user by id
 @app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      return make_response(jsonify({'user': user.json()}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
-    return make_response(jsonify({'message': 'error getting user'}), 500)
+def show(id: int):
+    u = User.query.get_or_404(id)
+    return jsonify(u.serialize())
 
 # update a user
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      data = request.get_json()
-      user.username = data['username']
-      user.email = data['email']
-      db.session.commit()
-      return make_response(jsonify({'message': 'user updated'}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
-    return make_response(jsonify({'message': 'error updating user'}), 500)
-  
-
-
 @app.route('/users/<int:id>', methods=['PATCH', 'PUT'])
 def update(id: int):
     u = User.query.get_or_404(id)
@@ -126,12 +94,10 @@ def update(id: int):
 # delete a user
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      db.session.delete(user)
-      db.session.commit()
-      return make_response(jsonify({'message': 'user deleted'}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except:
-    return make_response(jsonify({'message': 'error deleting user'}), 500)
+    try:
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'user deleted'}), 200)
+    except:
+        return make_response(jsonify({'message': 'error deleting user'}), 500)
