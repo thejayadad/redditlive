630d3f603256edcd4d7d7daca2682fe48b4cc1e7
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-06 17:54:34 -0400

    update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import traceback
 import sqlalchemy
+import hashlib
+import secrets
 
 
 
@@ -11,18 +13,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
 
+def scramble(password: str):
+    """Hash and salt the given password"""
+    salt = secrets.token_hex(16)
+    return hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
-    def json(self):
-        return {'id': self.id,'username': self.username, 'email': self.email}
-    
+    def __init__(self, username: str, password: str):
+            self.username = username
+            self.password = password
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+    }
 
 db.create_all()
+
 
 
 
@@ -38,13 +50,13 @@ def test():
 # create a user
 @app.route('/users', methods=['POST'])
 def create():
-    if 'username' not in request.json or 'email' not in request.json:
+    if 'username' not in request.json or 'password' not in request.json:
         return abort(400)
-    if len(request.json['username']) < 3 or len(request.json['email']) < 8:
+    if len(request.json['username']) < 3 or len(request.json['password']) < 8:
         return abort(400)
     u = User(
         username=request.json['username'],
-        email=request.json['email'],
+        password=scramble(request.json['password'])
 
     )
     db.session.add(u)
@@ -54,12 +66,13 @@ def create():
 
 # get all users
 @app.route('/users', methods=['GET'])
-def get_users():
-  try:
+def index():
     users = User.query.all()
-    return make_response(jsonify([user.json() for user in users]), 200)
-  except:
-    return make_response(jsonify({'message': 'error getting users'}), 500)
+    result = []
+    for u in users:
+        result.append(u.serialize())
+    return jsonify(result)
+
 
 # get a user by id
 @app.route('/users/<int:id>', methods=['GET'])
@@ -86,6 +99,29 @@ def update_user(id):
     return make_response(jsonify({'message': 'user not found'}), 404)
   except:
     return make_response(jsonify({'message': 'error updating user'}), 500)
+  
+
+
+@app.route('/users/<int:id>', methods=['PATCH', 'PUT'])
+def update(id: int):
+    u = User.query.get_or_404(id)
+    if 'username' not in request.json and 'password' not in request.json:
+        return abort(400)
+    if 'username' in request.json:
+        username = request.json['username']
+        if len(username) < 3:
+            return abort(400)
+        u.username = username
+    if 'password' in request.json:
+        password = request.json['password']
+        if len(password) < 8:
+            return abort(400)
+        u.password = scramble(password)
+    try:
+        db.session.commit()
+        return jsonify(u.serialize())
+    except:
+        return jsonify(False)
 
 # delete a user
 @app.route('/users/<int:id>', methods=['DELETE'])
